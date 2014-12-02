1b444f7ebf5285b23656a958319212c79828472d
Przemek Marciniak <przemek@ipokervip.net>
2014-12-02 11:31:19 +0000

    satellizer config

--- www/js/app.js
+++ www/js/app.js
@@ -33,3 +33,43 @@ APP.run(function($ionicPlatform) {
     });
 
 })
+
+.config(function($authProvider) {
+
+    $authProvider.facebook({
+      clientId: '624059410963642'
+    });
+
+    $authProvider.google({
+      clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
+    });
+
+    $authProvider.github({
+      clientId: '0ba2600b1dbdb756688b'
+    });
+
+    $authProvider.linkedin({
+      clientId: '77cw786yignpzj'
+    });
+
+    $authProvider.yahoo({
+      clientId: 'dj0yJmk9dkNGM0RTOHpOM0ZsJmQ9WVdrOVlVTm9hVk0wTkRRbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0wMA--'
+    });
+
+    $authProvider.live({
+      clientId: '000000004C12E68D'
+    });
+
+    $authProvider.twitter({
+      url: '/auth/twitter'
+    });
+
+    $authProvider.oauth2({
+      name: 'foursquare',
+      url: '/auth/foursquare',
+      redirectUri: window.location.origin,
+      clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
+      authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate',
+    });
+
+  });
