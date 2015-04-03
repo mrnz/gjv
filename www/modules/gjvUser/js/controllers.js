4c25123b0d9bc60f43206f067c42b549ae9f8976
Przemek <mrnz@wp.pl>
2015-04-03 14:28:39 +0100

    My7 new comm

--- www/modules/gjvUser/js/controllers.js
+++ www/modules/gjvUser/js/controllers.js
@@ -1,15 +1,29 @@
 angular.module('gjvUser')
 
-.controller('StartCtrl', function($scope){
+.controller('StartCtrl', function($scope, $state){
+	
+	$scope.join = function(){
+		$state.go('join'); 
+	}
+
+	$scope.forgotPassword = function(){
+		$state.go('forgotpassword');
+	}
 
 })
 
-.controller('JoinCtrl', function($scope){
-
+.controller('JoinCtrl', function($scope, $state){
+	$scope.cancel = function(){
+		$state.go('start');
+	}
 })
 
-.controller('ForgotPasswordCtrl', function($scope){
-
+.controller('ForgotPasswordCtrl', function($scope, $state){
+	
+	$scope.cancel = function(){
+		$state.go('start');
+	}
+	
 })
 
 
