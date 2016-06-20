206a2f78a5a2e58a0cc103d693a0dbb092d9469d
Przemek <mrnz@wp.pl>
2016-06-20 19:22:54 +0100

    lint info ctrl

--- www/js/controllers/infoCtrl.js
+++ www/js/controllers/infoCtrl.js
@@ -15,17 +15,17 @@ angular.module('starter')
 		$scope.info = [];
 		$scope.ready = false;
 		$scope.title = brandName; 
-		$scope.items = []
+		$scope.items = [];
 		dataFactory.info( brandName, modelName, volumeName ).then(function (result){
 			$scope.ready = true;
 			$scope.info = result.info; 
-			var base = 'http://www.gdziejestvin.pl/file/'
+			var base = 'http://www.gdziejestvin.pl/file/';
 			var foto1 = base + result.info.foto1;
 			var foto2 = base + result.info.foto2;
 			$scope.items = [
 				{src: foto1, sub: 'sub 1'},
 				{src: foto2, sub: 'sub 2'}
-			]
+			];
 		},function error (reason){console.log(reason);});
 
 
