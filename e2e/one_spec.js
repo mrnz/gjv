6b392d786a7013ff556116a8e2ae53ae8f6223d2
Przemek <mrnz@wp.pl>
2016-06-28 08:30:07 +0100

    travis test

--- e2e/one_spec.js
+++ e2e/one_spec.js
@@ -1,9 +1,18 @@
 describe('Testing the dash tab', function() {
+    
+    beforeEach(function() {
+      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+    });
+
     it('should be able to see the displayed products', function(done) {
       
       browser.waitForAngular();
       element( by.id('login-button') ).click();
 			done()
-    	
      });
+
+    afterEach(function() {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });    
 });
